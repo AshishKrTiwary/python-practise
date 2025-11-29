01fc4f8a62c59a868a7a5ca5fc4682293e616d29
AshishKrTiwary <ashishworkforfree@gmail.com>
2025-11-29 16:00:45 +0530

    maths module

---
+++ math-module.py
@@ -0,0 +1,8 @@
+import math
+import datetime
+
+print(math.factorial(5))
+
+print(math.sqrt(4))
+
+print(datetime.date.today())
---
+++ matplotlib-module.py
@@ -0,0 +1,5 @@
+pip3 install matplotlib
+
+import matplotlib.pyplot as plt
+
+plt.bar([0.25,1.25,3.25,4.35],[50,40,70,80,20],label="BMW",width=.5)
